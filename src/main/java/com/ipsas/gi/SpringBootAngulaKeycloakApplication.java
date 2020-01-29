574ca780c0119ccdd09699246acf03efbc0ef05c
TuemoT <thomastuemo@yahoo.fr>
2020-01-29 13:17:23 +0100

    mon premier commit

---
+++ src/main/java/com/ipsas/gi/SpringBootAngulaKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.ipsas.gi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngulaKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngulaKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipsas/gi/SpringBootAngulaKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipsas.gi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngulaKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
