72978ed542ef631e80d20ff6641b4c3ccfab1869
TuemoT <thomastuemo@yahoo.fr>
2020-01-30 11:37:45 +0100

    commit

--- Serveur/src/app/app.module.ts
+++ Serveur/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateClientComponent } from './create-client/create-client.component';
+import { EmployeeClientComponent } from './employee-client/employee-client.component';
+import { ClientDetailsComponent } from './client-details/client-details.component';
+import { ClientListComponent } from './client-list/client-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateClientComponent,
+    EmployeeClientComponent,
+    ClientDetailsComponent,
+    ClientListComponent
   ],
   imports: [
     BrowserModule,
